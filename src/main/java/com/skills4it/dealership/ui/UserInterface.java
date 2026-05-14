273cf8636590bff8d45a31671bfb549d8e9fe1de
Kenny Sosa <kennysosa2000@gmail.com>
2026-05-14 14:10:53 -0700

    welcome message

--- src/main/java/com/skills4it/dealership/ui/UserInterface.java
+++ src/main/java/com/skills4it/dealership/ui/UserInterface.java
@@ -41,7 +41,7 @@ public class UserInterface {
     private void displayHeader() {
         System.out.println();
         System.out.println("=============================================");
-        System.out.println("Welcome to the best dealership: ");
+        System.out.println("Welcome to the perfect dealership: ");
         System.out.println(dealership.getName());
         System.out.println(dealership.getAddress() + " | " + dealership.getPhone());
         System.out.println("=============================================");
