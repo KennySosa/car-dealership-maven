15dd291b441e1c38a89ed6f6c4b953b242566d7f
Kenny Sosa <kennysosa2000@gmail.com>
2026-05-14 14:00:25 -0700

    added new branch and welcomme msg

--- src/main/java/com/skills4it/dealership/ui/UserInterface.java
+++ src/main/java/com/skills4it/dealership/ui/UserInterface.java
@@ -37,7 +37,7 @@ public class UserInterface {
     private void init() {
         this.dealership = fileManager.getDealership();
     }
-//HELLOE AGAIN HI
+//HELLOE AGAIN HI AND AGAIN
     private void displayHeader() {
         System.out.println();
         System.out.println("=============================================");
